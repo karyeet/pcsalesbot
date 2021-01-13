02123cf0ac66d61af5fc4d241b0bed00692f392a
Kareem <kareem.shehada@gmail.com>
2021-01-13 01:56:16 -0800

    fix json detection

--- bot.js
+++ bot.js
@@ -105,7 +105,7 @@ const doNotsendIfMoreThan15MinOld = false
 
 function isJson(str) {
   try {
-      rJSON.parse(str);
+      JSON.parse(str);
   } catch (e) {
       return false;
   }
@@ -115,7 +115,7 @@ function isJson(str) {
 function getData(){
 request('https://www.reddit.com/r/buildapcsales/new.json?sort=new',cb1)
 function cb1(_,b){
-  if(!b || !isJson(b)){sessionData.failedPolls++; return}
+  if(!b || !b.body ||!isJson(b.body)){sessionData.failedPolls++; return}
   sessionData.successfulPolls++
   var data=JSON.parse(b.body).data.children['0'].data
   //console.log(data)

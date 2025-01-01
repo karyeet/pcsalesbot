0ddaadbc3bb7cd19301a80a164177bcd6306c94d
Kareem <28524112+karyeet@users.noreply.github.com>
2024-12-31 22:51:03 -0800

    ban unban term commands

---
+++ src/commands/ban_term.ts
@@ -0,0 +1,36 @@
+import {ChatInputCommandInteraction, SlashCommandBuilder} from 'discord.js';
+import type {SalesBot} from '../classes/SalesBot';
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ban_term')
+    .setDescription(
+      "Don't notify you if this term is in the title. Prioritized over tracked terms. Not case sensitive",
+    )
+    .addStringOption(option =>
+      option
+        .setName('term')
+        .setDescription('Term to ban')
+        .setRequired(true)
+        .setMaxLength(30),
+    ),
+  async execute(interaction: ChatInputCommandInteraction, salesbot: SalesBot) {
+    if (!interaction.guildId) {
+      await interaction.reply('This command only works in servers');
+      return false;
+    }
+
+    const term = interaction.options.getString('term')!.toLowerCase();
+    await salesbot.add_banned_term(
+      interaction.guildId,
+      interaction.user.id,
+      term,
+    );
+    await interaction.reply({
+      content: `Added term \`${term}\` to your banned terms`,
+      ephemeral: true,
+    });
+
+    return true;
+  },
+};
---
+++ src/commands/unban_term.ts
@@ -0,0 +1,41 @@
+import {ChatInputCommandInteraction, SlashCommandBuilder} from 'discord.js';
+import type {SalesBot} from '../classes/SalesBot';
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('unban_term')
+    .setDescription('Unban term. Not case sensitive')
+    .addStringOption(option =>
+      option
+        .setName('term')
+        .setDescription('Term to unban')
+        .setRequired(true)
+        .setMaxLength(30),
+    ),
+  async execute(interaction: ChatInputCommandInteraction, salesbot: SalesBot) {
+    if (!interaction.guildId) {
+      await interaction.reply('This command only works in servers');
+      return false;
+    }
+
+    const term = interaction.options.getString('term')!.toLowerCase();
+    const removed = await salesbot.remove_banned_term(
+      interaction.guildId,
+      interaction.user.id,
+      term,
+    );
+    if (removed === false) {
+      await interaction.reply({
+        content: `\`${term}\` is not in your banned terms`,
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: `Removed term \`${term}\` from your banned terms`,
+        ephemeral: true,
+      });
+    }
+
+    return true;
+  },
+};
